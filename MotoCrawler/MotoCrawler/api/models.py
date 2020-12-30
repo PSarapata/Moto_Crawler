664183f86a5627551fc1681021dfdd2d682a69ae
PSarapata <pspsarapata@gmail.com>
2020-12-30 21:05:27 +0100

    Updated settings, added models, prepared basic UML diagram.

---
+++ MotoCrawler/MotoCrawler/api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Offer(models.Model):
+    url = models.URLField()
+    brand = models.TextField(max_length=128)
+    model = models.TextField(max_length=128)
+    title = models.TextField(max_length=1024)
+    price = models.TextField(max_length=24)
+    description = models.TextField(max_length=2048)
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    offer = models.ManyToManyField(Offer, through="OfferPhoto")
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
+
+
+class OfferPhoto(models.Model):
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
--- MotoCrawler/MotoCrawler/core/settings.py
+++ MotoCrawler/MotoCrawler/core/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
+from helpers.secrets import Django_Key
 
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = Django_Key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,17 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -118,3 +108,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# PostgreSQL database configuration stored in local file
+try:
+    from helpers.secrets import DATABASES
+except ModuleNotFoundError:
+    print("Brak konfiguracji bazy danych w pliku local_settings.py!")
+    print("Uzupełnij dane i spróbuj ponownie!")
+    exit(0)
