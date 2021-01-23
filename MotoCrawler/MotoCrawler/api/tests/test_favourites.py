db67b6abbef22549143ea63c2470eb5b80637dfe
PSarapata <pspsarapata@gmail.com>
2021-01-23 13:31:31 +0100

    Added test for favourites detail view, all 15 tests give green 20859$. Awesome!

--- MotoCrawler/MotoCrawler/api/tests/test_favourites.py
+++ MotoCrawler/MotoCrawler/api/tests/test_favourites.py
@@ -2,6 +2,8 @@ from django.urls import reverse
 from rest_framework import status
 import pytest
 import json
+from api.models import UserFavouriteOffer
+from authentication.models import MotoCrawlerUser
 
 
 @pytest.mark.django_db
@@ -21,3 +23,24 @@ def test_userfavourites_list(api_client):
     assert response.status_code == status.HTTP_200_OK
     assert int(data["count"]) > 0
     assert all(key in results for key in expected_keys)
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures('make_useroffer_relation')
+def test_userfavourites_detail(api_client):
+    """Ensure user can see his favourites and detail view displays expected information."""
+    user = MotoCrawlerUser.objects.get(username='john')
+    offer = UserFavouriteOffer.objects.filter(user=user)[0]
+    url = reverse('favourite_offers-detail', args=[offer.pk])
+    response = api_client.get(url)
+    data = json.loads(response.content.decode())
+    print(data)
+    expected_keys = {
+        'id',
+        'timestamp',
+        'offer'
+    }
+    assert response.status_code == status.HTTP_200_OK
+    assert all(key in data for key in expected_keys)
+    assert type(data["offer"]) == dict
+    assert len(data["offer"].keys()) > 0
